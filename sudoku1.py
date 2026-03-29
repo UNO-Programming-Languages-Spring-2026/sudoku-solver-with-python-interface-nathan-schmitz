cde9ad6b2300266a204aa13e1fd0ffda53659ab0
Nathan-schmitz01 <schmitznathan38@gmail.com>
2026-03-29 08:51:10 -0500

    Solved Problem 1b

--- sudoku1.py
+++ sudoku1.py
@@ -11,5 +11,10 @@ class ClingoApp(clingo.application.Application):
         
         ctl.ground()
         ctl.solve()
+    
+    def print_model(self, model, printer) -> None:
+        symbols = sorted(model.symbols(shown=True))
+        print(" ".join(str(s) for s in symbols))
+        sys.stdout.flush()
 
 clingo.application.clingo_main(ClingoApp())
